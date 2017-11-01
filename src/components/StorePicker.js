f2da573cd5326c5995ce9b678fd0e5a2301a2414
Iliyan Ganev <ganeviliyan@gmail.com>
2017-11-01 14:13:54 -0700

    Add modules, props and start with state

---
+++ src/components/StorePicker.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { getFunName } from '../helpers';
+
+class StorePicker extends React.Component {
+
+	// constructor(){
+	// 	super();
+	// 	this.goToStore = this.goToStore.bind(this);
+	// }
+
+	goToStore(event) {
+		event.preventDefault();
+		console.log("You submitted the form!!!!!")
+		console.log(this.storeInput.value);
+		const storeId = this.storeInput.value
+		console.log(`Going to ${storeId}`);
+		this.context.router.transitionTo(`/store/${storeId}`);
+	}
+	render() {
+		// Comment anywhere outside of jsx.
+		return (
+			<form className="store-selector" onSubmit={this.goToStore.bind(this)}>
+			{/*This is a jsx comment*/}
+				<h2>Please Enter A Store</h2>
+				<input type="text" required placeholder="Store Name" defaultValue={getFunName()} ref={(input) => { this.storeInput = input}} />
+				<button type="submit">Visit Store  →</button>
+			</form>
+		)
+	}
+}
+
+StorePicker.contextTypes = {
+	router: React.PropTypes.object
+}
+
+export default StorePicker;
