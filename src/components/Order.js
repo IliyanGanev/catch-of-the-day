2dcbbe75a53ca614e28efc93434556df33761b3a
Iliyan Ganev <ganeviliyan@gmail.com>
2017-11-07 17:48:54 -0800

    Create user auth

--- src/components/Order.js
+++ src/components/Order.js
@@ -19,7 +19,18 @@ class Order extends React.Component {
 		} 
 		return (
 			<li key={key}>
-				<span>{count}lbs {fish.name} {removeButton}</span>
+				<span>
+				<CSSTransitionGroup
+					component="span"
+					className="count"
+					transitionName="count"
+					transitionEnterTimeout={250}
+					transitionLeaveTimeout={250} 
+				>
+				<span key={count}>{count}</span>
+				</CSSTransitionGroup>
+				lbs {fish.name} {removeButton}
+				</span>
 				<span className="price">{formatPrice(count * fish.price)}</span>
 			</li>
 		)
@@ -59,5 +70,11 @@ class Order extends React.Component {
 		)
 	}
 }
+
+Order.propTypes = {
+	fishes: React.PropTypes.object.isRequired,
+	order: React.PropTypes.object.isRequired,
+	removeFromOrder: React.PropTypes.func.isRequired
+};
 
 export default Order;
